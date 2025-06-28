f18bc887cdf50f2569be5ef29ba5db2458c28f81
lenardjombo <leonardjombo6@gmail.com>
2025-06-28 18:16:21 +0300

    feat: Improved test cases to include more test cases

--- tests/ArraysAndHashing/contains_duplicate_test.go
+++ tests/ArraysAndHashing/contains_duplicate_test.go
@@ -1,14 +1,49 @@
-package arraysandhashing
+package arraysandhashing_test
 
-import ("testing"
-		"github.com/lenardjombo/blind75/solutions/ArraysAndHashing"
-	)
-func TestDuplicates(t *testing.T){
-	input := []int{1,2,3,4,5,6,7,8,9}
-	expected := false
-	result := arraysandhashing.ContainsDuplicate(input)
+import (
+	"testing"
 
-	if result != expected{
-		t.Errorf("Contains Duplicates (%v) = %v;want %v",input,result,expected)
-	}
+	"github.com/lenardjombo/blind75/solutions/ArraysAndHashing"
+)
+
+func TestContainsDuplicates(t *testing.T){
+	// Test cases definition
+	testCases := []struct{
+		name string
+		numbers []int
+		expected bool
+	}{
+		{
+		name: "Array with no duplicates",
+		numbers: []int{1,2,3,4,5},
+		expected: false,
+	},
+	{
+		name: "Array with duplicates",
+		numbers: []int{1,2,3,2},
+		expected:true,
+	},
+	{
+		name: "Empty array",
+		numbers: []int{},
+		expected:false,
+	},
+	{
+		name: "Array with one value",
+		numbers: []int{99},
+		expected:false,
+	},
+}
+
+//Run each test case one by one
+for _, tc := range testCases {
+	t.Run(tc.name,func(t *testing.T) {
+		result := arraysandhashing.ContainsDuplicate(tc.numbers)
+
+		//Check if results == expected
+		if result != tc.expected{
+			t.Errorf("For numbers %v:\nGot %t\nBut expected %t",tc.numbers,result,tc.expected)
+		}
+	})
+}
 }
